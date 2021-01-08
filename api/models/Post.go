a2084ab01d6251f37fc9f82c89473dd49cf653f1
Funskie <tusty9292@gmail.com>
2021-01-08 15:16:46 +0800

    Fix post get user model bug

--- api/models/Post.go
+++ api/models/Post.go
@@ -13,7 +13,7 @@ type Post struct {
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	Title     string    `gorm:"size:255;not null;" json:"title"`
 	Content   string    `gorm:"text;not null;" json:"content"`
-	User      User      `gorm:"foreignkey:AuthorID" json:"author"`
+	Author    User      `gorm:"foreignkey:AuthorID" json:"author"`
 	AuthorID  uint32    `gorm:"not null" json:"author_id"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
@@ -23,7 +23,7 @@ func (p *Post) Prepare() {
 	p.ID = 0
 	p.Title = html.EscapeString(strings.TrimSpace(p.Title))
 	p.Content = html.EscapeString(strings.TrimSpace(p.Content))
-	p.User = User{}
+	p.Author = User{}
 	p.CreatedAt = time.Now()
 	p.UpdatedAt = time.Now()
 }
@@ -63,7 +63,7 @@ func (p *Post) FindAllPosts(db *gorm.DB) (*[]Post, error) {
 	}
 	if len(posts) > 0 {
 		for i := range posts {
-			err = db.Debug().First(&User{}, posts[i].AuthorID).Error
+			err := db.Debug().Model(&User{}).Where("id = ?", posts[i].AuthorID).Take(&posts[i].Author).Error
 			if err != nil {
 				return &[]Post{}, err
 			}
@@ -78,7 +78,7 @@ func (p *Post) FindPostByID(db *gorm.DB, pid uint64) (*Post, error) {
 		return &Post{}, err
 	}
 	if p.ID != 0 {
-		err = db.Debug().First(&User{}, p.AuthorID).Error
+		err = db.Debug().Model(&User{}).Where("id = ?", p.AuthorID).Take(&p.Author).Error
 		if err != nil {
 			return &Post{}, err
 		}
